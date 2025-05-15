88cc3f41f81d3981d1a593f4817dccbfb37eb56a
Aditya Bhosle <adityabhsl83@gmail.com>
2025-05-15 23:21:03 +0530

    my first commit for cypress

---
+++ cypress/integration/examples/test1.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+describe("My First Suite", function () {
+  it("My First Test Case!", function () {
+    // steps
+    cy.visit("/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.get(".product:visible").should("have.length", 4);
+    cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+
+    cy.get(".products")
+      .find(".product")
+      .each(($e1, index, $list) => {
+        const textVeg = $e1.find("h4.product-name").text();
+        if (textVeg.includes("Cashews")) {
+          cy.wrap($e1).find("button").click();
+        }
+      });
+
+    cy.get(".brand").then(function (logoText) {
+      cy.log(logoText.text());
+    });
+  });
+});
---
+++ cypress/integration/examples/test2.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+describe("Another Suite", function () {
+  it("Checking Advanced Cypress", function () {
+    // steps
+    cy.visit("/AutomationPractice");
+  });
+});
