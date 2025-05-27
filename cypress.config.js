a4db503084e7d8fec8d364d038aad7d860c4deda
Aditya Bhosle <adityabhsl83@gmail.com>
2025-05-27 11:28:11 +0530

    installed new packages and custom scrip commands

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
     },
     specPattern: "cypress/integration/examples/*.js",
